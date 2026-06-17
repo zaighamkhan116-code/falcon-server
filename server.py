155a1cc679dbabf1cd4ade33f818cf2278cea7bb
zaighamkhan116-code <zaighamkhan116@gmail.com>
2026-06-17 13:31:27 +0500

    Update server.py

--- server.py
+++ server.py
@@ -155,17 +155,19 @@ def panel():
             """, (acc, status))
             conn.commit()
 
-    c.execute("""
-    SELECT c.account, c.status,
-           IFNULL(p.balance,0),
-           IFNULL(p.profit,0)
-    FROM clients c
-    LEFT JOIN profits p ON c.account = p.account
-    """)
+        c.execute("""
+        SELECT c.account, c.status,
+               IFNULL(p.balance,0),
+               IFNULL(p.profit,0)
+        FROM clients c
+        LEFT JOIN profits p ON c.account = p.account
+        """)
 
-    data = c.fetchall()
-    print("PANEL DATA =", data)
-    conn.close()
+        data = c.fetchall()
+
+print("PANEL DATA =", data)
+
+conn.close()
 
     total_daily = sum([row[3] for row in data])
     total_weekly = total_daily
