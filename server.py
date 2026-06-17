0f5c0e4e7e5d015a8207b34cb097551f526b5a63
zaighamkhan116-code <zaighamkhan116@gmail.com>
2026-06-18 02:49:43 +0500

    Update server.py

--- server.py
+++ server.py
@@ -98,7 +98,7 @@ def check():
         if not data:
             return jsonify({"status": "blocked"})
 
-        acc = str(data.get("account"))
+        acc = str(data.get("acc = str(data.get("account")).strip()"))
 
         conn = sqlite3.connect("data.db")
         c = conn.cursor()
